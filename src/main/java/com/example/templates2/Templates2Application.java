23e62196ef8168471f03e7ac68443e85813020e4
yr <aaaa@a.a>
2024-12-27 14:59:25 +0900

    레이아웃

---
+++ src/main/java/com/example/templates2/Templates2Application.java
@@ -0,0 +1,13 @@
+package com.example.templates2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Templates2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Templates2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/templates2/Templates2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templates2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Templates2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
